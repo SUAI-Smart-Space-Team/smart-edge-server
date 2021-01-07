b34a86503be2b08b0b5fa40687fe15dc9431cb1a
Nikita Yankovskii <yannikita98@gmail.com>
2021-01-07 03:46:51 +0300

    skeleton code

---
+++ sitesuai/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace sitesuai.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+            return View();
+        }
+
+        [HttpPost]
+        public RedirectResult SetValue(string coolerSpeed, string coolerColor)
+        {
+            //TODO: write to db
+            System.Diagnostics.Debug.WriteLine(coolerSpeed);
+            return Redirect("Index");
+        }
+
+        [HttpPost]
+        public RedirectResult SetPValue(string pumpSpeed, string pumpColor)
+        {
+            //TODO: write to db
+            System.Diagnostics.Debug.WriteLine(pumpSpeed);
+            return Redirect("Index");
+        }
+    }
+}
---
+++ sitesuai/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="sitesuai.MvcApplication" Language="C#" %>
